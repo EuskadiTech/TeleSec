92d3522f98952c51037971cdb3a8472c99b42ef0
naielv <109038805+naielv@users.noreply.github.com>
2025-07-18 13:26:18 +0200

    fix

--- dist/static/gun.js
+++ dist/static/gun.js
@@ -1827,7 +1827,7 @@
 			root.on('put', function(msg){
 				this.to.next(msg); // remember to call next middleware adapter
 				var put = msg.put, soul = put['#'], key = put['.'], id = msg['#'], ok = msg.ok||'', tmp; // pull data off wire envelope
-				if (!(soul.startsWith(TABLE))) return; // fix https://github.com/amark/gun/issues/1377
+				if (!(root.next || '')[soul]){ return } // fix https://github.com/amark/gun/issues/1377
 				disk[soul] = Gun.state.ify(disk[soul], key, put['>'], put[':'], soul); // merge into disk object
 				if(stop && size > (4999880)){ root.on('in', {'@': id, err: "localStorage max!"}); return; }
 				//if(!msg['@']){ acks.push(id) } // then ack any non-ack write. // TODO: use batch id.
