fe5c54ff3e96a737cbc2e71049a86ad0107bcb7b
naielv <109038805+naielv@users.noreply.github.com>
2025-08-11 21:59:23 +0200

    update

--- src/page/index.js
+++ src/page/index.js
@@ -1,7 +1,7 @@
 PAGES.index = {
   //navcss: "btn1",
   Title: "Inicio",
-  index: function () {
+  index: function() {
     container.innerHTML = `
                 <h1>¡Hola, ${SUB_LOGGED_IN_DETAILS.Nombre}!</h1>
                 <em>Utiliza el menú superior para abrir un modulo</em>
@@ -9,4 +9,22 @@ PAGES.index = {
                 <button class="btn1" onclick="LogOutTeleSec()">Cerrar sesión</button>
             `;
   },
+  edit: function(mid) {
+    var qrscan = safeuuid()
+    container.innerHTML = `
+      <h1>Buscar con Aztec</h1>
+      <div id="${qrscan}"></div>
+      <br><br>`;
+    const html5QrCode = new Html5Qrcode(
+      qrscan, { formatsToSupport: [Html5QrcodeSupportedFormats.AZTEC] });
+    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+      /* handle success */
+      alert(decodedText)
+      html5QrCode.stop()
+    };
+    const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+    
+    // If you want to prefer front camera
+    html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+  }
 };
