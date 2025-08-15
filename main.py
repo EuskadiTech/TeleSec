dbddd70ef9d8b0ca1f41a402de6d17c165da3c02
naielv <109038805+naielv@users.noreply.github.com>
2025-08-15 17:30:16 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import webview
+import sys
 
 if __name__ == '__main__':
-    webview.create_window('TeleSec', 'dist/index.html')
+    webview.create_window('TeleSec', sys._MEIPASS + '/dist/index.html')
     webview.start(ssl=False, icon="dist/icon512_maskable.png")
