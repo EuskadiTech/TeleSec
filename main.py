ccefabbc4a99f709f502d62e3d85d2ceea0da3fc
Naiel <109038805+naielv@users.noreply.github.com>
2025-08-15 16:46:56 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import webview
+
+if __name__ == '__main__':
+    webview.create_window('TeleSec', 'dist/index.html')
+    # HTTP server is started automatically for local relative paths
+    webview.start(ssl=True)
